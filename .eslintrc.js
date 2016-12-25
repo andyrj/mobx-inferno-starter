ea33a79537f5c0a401fbd7efaf34eeffad518a2b
Andy Johnson <andyrjohnson82@gmail.com>
2016-12-25 18:15:42 +0000

    added code-smell eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,39 @@ module.exports = {
     "plugins": [
         "standard",
         "promise",
-        "inferno"
+        "inferno",
+		"smells"
     ],
     "parser": "babel-eslint",
     "settings": {
-      "inferno": {
-        "pragma": "Inferno", 
-        "version": "1.0"
-      }
+    	"inferno": {
+    		"pragma": "Inferno", 
+			"version": "1.0"
+      	}
     },
     "parserOptions": {
-      "ecmaFeatures": {
-        "jsx": true,
-        "modules": true
-      }
+    	"ecmaFeatures": {
+    		"jsx": true,
+    		"modules": true
+      	}
     },
     "rules": {
-      "strict": 0,
-      "eqeqeq": [2, "smart"],
-      "no-unused-vars": 0,
-	  "max-len": ["error", 80],
-      "space-before-function-paren": 0,
-      "inferno/display-name": 0,
-      "inferno/require-optimization": 0,
-      "inferno/prop-types": 0
+		"strict": 0,
+		"eqeqeq": [2, "smart"],
+		"no-unused-vars": 0,
+		"max-len": ["error", 80],
+		"space-before-function-paren": 0,
+		"max-statements": [2, 16],
+		"max-depth":[2, 5],
+		"complexity": [2, 7],
+		"max-params":[2,2],
+		"max-nested-callbacks":[2,1],
+		"smells/no-switch": 1,
+		"smells/no-complex-switch-case": 1,
+		"smells/no-setinterval": 1,
+		"smells/no-this-assign": 1,
+		"inferno/display-name": 0,
+		"inferno/require-optimization": 0,
+		"inferno/prop-types": 0
     }
 };
